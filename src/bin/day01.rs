7445d683e529325c37c05df2036efd5a962345e4
phire <me@phire.cc>
2022-12-01 12:11:02 +0100

    refactor and set up tests

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,6 +1,8 @@
+use std::io::BufRead;
+
 use anyhow::Result;
 
-fn main() -> Result<()> {
+fn solve<T: BufRead>(input: T) -> Result<Vec<i64>> {
     let mut cur = 0;
     let mut top3 = [0; 3];
     let mut update = |cur| {
@@ -14,7 +16,7 @@ fn main() -> Result<()> {
             }
         }
     };
-    for line in std::io::stdin().lines() {
+    for line in input.lines() {
         let l = line?;
         if l.is_empty() {
             update(cur);
@@ -24,7 +26,25 @@ fn main() -> Result<()> {
         }
     }
     update(cur);
-    println!("part 1: {}", top3[2]);
-    println!("part 2: {}", top3.iter().sum::<i64>());
+    Ok(vec![top3[2], top3.iter().sum::<i64>()])
+}
+
+fn main() -> Result<()> {
+    for (i, s) in solve(std::io::stdin().lock())?.iter().enumerate() {
+        println!("part {}: {}", i+1, s);
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn example() {
+        assert_eq!(solve(include_bytes!("../../data/day01_example.txt").as_slice()).unwrap(), [24000, 45000]);
+    }
+    #[test]
+    fn input() {
+        assert_eq!(solve(include_bytes!("../../data/day01_input.txt").as_slice()).unwrap(), [72478, 210367]);
+    }
 }
