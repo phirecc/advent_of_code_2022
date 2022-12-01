7baa974cdacad0e51ad1dde2e776ec49217c0547
phire <me@phire.cc>
2022-12-01 10:30:12 +0100

    day01 part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -2,17 +2,29 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     let mut cur = 0;
-    let mut best = 0;
+    let mut top3 = [0; 3];
+    let mut update = |cur| {
+        for i in 0..top3.len() {
+            if cur > top3[i] && (i == top3.len()-1 || cur < top3[i+1]) {
+                for k in 0..i {
+                    top3[k] = top3[k+1];
+                }
+                top3[i] = cur;
+                break;
+            }
+        }
+    };
     for line in std::io::stdin().lines() {
-        let l = &line?;
+        let l = line?;
         if l.is_empty() {
-            best = std::cmp::max(cur, best);
+            update(cur);
             cur = 0;
         } else {
-            let x: i64 = l.parse()?;
-            cur += x;
+            cur += l.parse::<i64>()?;
         }
     }
-    println!("part 1: {}", best);
+    update(cur);
+    println!("part 1: {}", top3[2]);
+    println!("part 2: {}", top3.iter().sum::<i64>());
     Ok(())
 }
