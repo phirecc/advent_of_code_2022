08492eaae0272ec0b524f1de95cd008c669db805
phire <me@phire.cc>
2022-12-03 10:38:29 +0100

    day03 part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,30 +1,50 @@
-use std::io::BufRead;
+use std::{io::BufRead, collections::{HashMap, HashSet}};
 
 use anyhow::Result;
 
+fn get_priority(c: char) -> i64 {
+    if c.is_ascii_lowercase() {
+        c as i64 - 'a' as i64 + 1
+    } else {
+        c as i64 - 'A' as i64 + 27
+    }
+}
+
 fn solve<T: BufRead>(input: T) -> Result<Vec<i64>> {
-    let mut sum = 0;
-    for line in input.lines() {
-        let mut seen = std::collections::HashSet::new();
-        let mut priorities = std::collections::HashSet::new();
+    let mut sum1 = 0;
+    let mut sum2 = 0;
+    let mut type_count: HashMap<char, i64> = HashMap::new();
+    for (lnum, line) in input.lines().enumerate() {
+        let mut seen = HashSet::new();
+        let mut priorities = HashSet::new();
         let l = line?;
-        for (i, c) in l.chars().enumerate() {
-            if i < l.len()/2 {
-                seen.insert(c);
-            } else {
-                if seen.contains(&c) {
-                    let x = if c.is_ascii_lowercase() {
-                        c as i64 - 'a' as i64 + 1
-                    } else {
-                        c as i64 - 'A' as i64 + 27
-                    };
-                    priorities.insert(x);
-                }
+        let first_half = &l[..l.len()/2];
+        let second_half = &l[l.len()/2..];
+        first_half.chars().for_each(|c| { seen.insert(c); });
+        for c in second_half.chars() {
+            if seen.contains(&c) {
+                priorities.insert(get_priority(c));
             }
         }
-        sum += priorities.iter().sum::<i64>();
+        sum1 += priorities.iter().sum::<i64>();
+
+        // part 2: also insert second half into seen
+        second_half.chars().for_each(|c| { seen.insert(c); });
+        for c in seen {
+            *type_count.entry(c).or_default() += 1;
+        }
+
+        if (lnum+1) % 3 == 0 {
+            for (k, v) in &type_count {
+                if *v == 3 {
+                    sum2 += get_priority(*k);
+                    break;
+                }
+            }
+            type_count.clear();
+        }
     }
-    Ok(vec![sum])
+    Ok(vec![sum1, sum2])
 }
 
 fn main() -> Result<()> {
@@ -39,10 +59,10 @@ mod test {
     use super::*;
     #[test]
     fn example() {
-        assert_eq!(solve(include_bytes!("../../data/day03_example.txt").as_slice()).unwrap(), [157]);
+        assert_eq!(solve(include_bytes!("../../data/day03_example.txt").as_slice()).unwrap(), [157, 70]);
     }
     #[test]
     fn input() {
-        assert_eq!(solve(include_bytes!("../../data/day03_input.txt").as_slice()).unwrap(), [7597]);
+        assert_eq!(solve(include_bytes!("../../data/day03_input.txt").as_slice()).unwrap(), [7597, 2607]);
     }
 }
