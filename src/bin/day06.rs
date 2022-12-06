cf803a453b66b7cfa95183d1b3c713ec75dc31e4
phire <me@phire.cc>
2022-12-06 09:43:46 +0100

    day06 part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -2,22 +2,19 @@ use std::{io::BufRead, collections::{HashMap, hash_map::Entry}};
 
 use anyhow::Result;
 
-fn solve<T: BufRead>(input: T) -> Result<Vec<i64>> {
-    let line = input.lines().next().unwrap()?;
+fn find_first_marker(s: &str, length: usize) -> Option<usize> {
     let mut seen: HashMap<char, i64> = HashMap::new();
-    let chars: Vec<_> = line.chars().collect();
-    let mut res1: i64 = -1;
+    let chars: Vec<_> = s.chars().collect();
     for (i, c) in chars.iter().enumerate() {
         seen.entry(*c).and_modify(|x| *x += 1).or_insert(1);
-        if i < 3 {
+        if i < length-1 {
             continue;
         }
 
-        if seen.len() == 4 {
-            res1 = i as i64 + 1;
-            break;
+        if seen.len() == length {
+            return Some(i);
         }
-        if let Entry::Occupied(mut o) = seen.entry(chars[i-3]) {
+        if let Entry::Occupied(mut o) = seen.entry(chars[i-(length-1)]) {
             let x = o.get_mut();
             *x -= 1;
             if *x == 0 {
@@ -25,7 +22,15 @@ fn solve<T: BufRead>(input: T) -> Result<Vec<i64>> {
             }
         }
     }
-    Ok(vec![res1])
+    None
+}
+
+fn solve<T: BufRead>(input: T) -> Result<Vec<usize>> {
+    let line = input.lines().next().unwrap()?;
+    Ok(vec![
+       find_first_marker(&line, 4).unwrap()+1,
+       find_first_marker(&line, 14).unwrap()+1,
+    ])
 }
 
 fn main() -> Result<()> {
@@ -40,10 +45,10 @@ mod test {
     use super::*;
     #[test]
     fn example() {
-        assert_eq!(solve(include_bytes!("../../data/day06_example.txt").as_slice()).unwrap(), [7]);
+        assert_eq!(solve(include_bytes!("../../data/day06_example.txt").as_slice()).unwrap(), [7, 19]);
     }
     #[test]
     fn input() {
-        assert_eq!(solve(include_bytes!("../../data/day06_input.txt").as_slice()).unwrap(), [1080]);
+        assert_eq!(solve(include_bytes!("../../data/day06_input.txt").as_slice()).unwrap(), [1080, 3645]);
     }
 }
