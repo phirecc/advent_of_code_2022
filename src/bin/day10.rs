d7cf524fbfc953725501f5654289c6245c114bcb
phire <me@phire.cc>
2022-12-10 09:15:46 +0100

    day10 part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -2,10 +2,11 @@ use std::io::BufRead;
 
 use anyhow::{Result, bail};
 
-fn solve<T: BufRead>(input: T) -> Result<Vec<i32>> {
+fn solve<T: BufRead>(input: T) -> Result<(i32, String)> {
     let mut cycle = 0;
     let mut x = 1;
     let mut part1 = 0;
+    let mut crt_buf = String::new();
     for line in input.lines() {
         let l = line?;
         let mut s = l.split_whitespace();
@@ -23,16 +24,24 @@ fn solve<T: BufRead>(input: T) -> Result<Vec<i32>> {
             if cycle % 40 == 20 {
                 part1 += cycle*x;
             }
+            if (x - (cycle-1) % 40).abs() <= 1 {
+                crt_buf.push('#');
+            } else {
+                crt_buf.push('.');
+            }
+            if cycle % 40 == 0 {
+                crt_buf.push('\n');
+            }
         }
         x = new_x;
     }
-    Ok(vec![part1])
+    Ok((part1, crt_buf))
 }
 
 fn main() -> Result<()> {
-    for (i, s) in solve(std::io::stdin().lock())?.iter().enumerate() {
-        println!("part {}: {}", i+1, s);
-    }
+    let sol = solve(std::io::stdin().lock())?;
+    println!("part {}: {}", 1, sol.0);
+    println!("part {}:\n{}", 2, sol.1);
     Ok(())
 }
 
@@ -41,10 +50,28 @@ mod test {
     use super::*;
     #[test]
     fn example() {
-        assert_eq!(solve(include_bytes!("../../data/day10_example.txt").as_slice()).unwrap(), [13140]);
+        let s =
+"\
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+";
+        assert_eq!(solve(include_bytes!("../../data/day10_example.txt").as_slice()).unwrap(), (13140, s.into()));
     }
     #[test]
     fn input() {
-        assert_eq!(solve(include_bytes!("../../data/day10_input.txt").as_slice()).unwrap(), [15360]);
+        let s =
+"\
+###..#..#.#....#..#...##..##..####..##..
+#..#.#..#.#....#..#....#.#..#....#.#..#.
+#..#.####.#....####....#.#......#..#..#.
+###..#..#.#....#..#....#.#.##..#...####.
+#....#..#.#....#..#.#..#.#..#.#....#..#.
+#....#..#.####.#..#..##...###.####.#..#.
+";
+        assert_eq!(solve(include_bytes!("../../data/day10_input.txt").as_slice()).unwrap(), (15360, s.into()));
     }
 }
