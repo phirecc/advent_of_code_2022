e354fd78f7247e93e021b1552f28457a8470f725
phire <me@phire.cc>
2022-12-11 12:58:15 +0100

    day11 part 2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -6,30 +6,31 @@ use anyhow::{Result, bail};
 
 #[derive(Default)]
 struct Monkey {
-    items: Vec<i32>,
-    inspect: Option<Box<dyn Fn(i32) -> i32>>,
-    next_monkey: Option<Box<dyn Fn(i32) -> i32>>,
+    items: Vec<usize>,
+    inspect: Option<Box<dyn Fn(usize) -> usize>>,
+    next_monkey: Option<Box<dyn Fn(usize) -> usize>>,
 }
 
-fn solve<T: BufRead>(input: T) -> Result<Vec<i32>> {
+fn solve<T: BufRead>(input: T) -> Result<Vec<usize>> {
     let mut monkeys = Vec::new();
     let mut cur_monkey: Monkey = Default::default();
     let mut lines = input.lines().peekable();
+    let mut common_div = 1;
     while let Some(line) = lines.next() {
         let l = line?;
         let t = l.trim();
         if t.starts_with("Starting items") {
-            let items: Vec<i32> = t.rsplit_once(": ").unwrap().1.split(", ").map(|x| x.parse().unwrap()).collect();
+            let items: Vec<usize> = t.rsplit_once(": ").unwrap().1.split(", ").map(|x| x.parse().unwrap()).collect();
             cur_monkey.items = items;
         } else if t.starts_with("Operation") {
             let formula: Vec<&str> = t.rsplit_once(" = ").unwrap().1.split(' ').collect();
-            let op: Box<dyn Fn(i32, i32) -> i32> = match formula[1] {
-                "+" => Box::new(i32::add),
-                "*" => Box::new(i32::mul),
+            let op: Box<dyn Fn(usize, usize) -> usize> = match formula[1] {
+                "+" => Box::new(usize::add),
+                "*" => Box::new(usize::mul),
                 _ => bail!("Invalid operation: {}", formula[1])
             };
-            let f: Box<dyn Fn(i32) -> i32> = {
-                let x = formula[2].parse::<i32>();
+            let f: Box<dyn Fn(usize) -> usize> = {
+                let x = formula[2].parse::<usize>();
                 if let Ok(x) = x {
                     Box::new(move |z| op(z, x))
                 } else {
@@ -38,8 +39,9 @@ fn solve<T: BufRead>(input: T) -> Result<Vec<i32>> {
             };
             cur_monkey.inspect = Some(f);
         } else if t.starts_with("Test") {
-            let x: i32 = t.rsplit_once(' ').unwrap().1.parse().unwrap();
-            let variants = lines.next_chunk::<2>().unwrap().map(|l| l.unwrap().trim().rsplit_once(' ').unwrap().1.parse::<i32>().unwrap());
+            let x: usize = t.rsplit_once(' ').unwrap().1.parse().unwrap();
+            common_div *= x;
+            let variants = lines.next_chunk::<2>().unwrap().map(|l| l.unwrap().trim().rsplit_once(' ').unwrap().1.parse::<usize>().unwrap());
             cur_monkey.next_monkey = Some(Box::new(move |z| if z % x == 0 {
                 variants[0]
             } else {
@@ -51,13 +53,17 @@ fn solve<T: BufRead>(input: T) -> Result<Vec<i32>> {
         }
     }
     monkeys.push(cur_monkey);
+    let mut values2 = Vec::new();
+    for m in &monkeys {
+        values2.push(m.items.clone());
+    }
     let mut inspections = vec![0; monkeys.len()];
     for _ in 0..20 {
         for i in 0..monkeys.len() {
             let mut m = std::mem::take(&mut monkeys[i]);
             for item in &m.items {
                 let new_value = m.inspect.as_ref().unwrap()(*item) / 3;
-                let next = m.next_monkey.as_ref().unwrap()(new_value) as usize;
+                let next = m.next_monkey.as_ref().unwrap()(new_value);
                 monkeys[next].items.push(new_value);
                 inspections[i] += 1;
             }
@@ -67,7 +73,25 @@ fn solve<T: BufRead>(input: T) -> Result<Vec<i32>> {
     }
     inspections.sort_by(|a, b| b.cmp(a));
     let monkey_business = inspections[0] * inspections[1];
-    Ok(vec![monkey_business])
+
+    // part 2
+    let mut inspections2 = vec![0; monkeys.len()];
+    for _ in 0..10000 {
+        for i in 0..monkeys.len() {
+            let m = &monkeys[i];
+            let v = std::mem::take(&mut values2[i]);
+            for item in v {
+                let new_value = m.inspect.as_ref().unwrap()(item) % common_div;
+                let next = m.next_monkey.as_ref().unwrap()(new_value);
+                values2[next].push(new_value);
+                inspections2[i] += 1;
+            }
+        }
+    }
+    inspections2.sort_by(|a, b| b.cmp(a));
+    let monkey_business2 = inspections2[0] * inspections2[1];
+
+    Ok(vec![monkey_business, monkey_business2])
 }
 
 fn main() -> Result<()> {
@@ -82,10 +106,10 @@ mod test {
     use super::*;
     #[test]
     fn example() {
-        assert_eq!(solve(include_bytes!("../../data/day11_example.txt").as_slice()).unwrap(), [10605]);
+        assert_eq!(solve(include_bytes!("../../data/day11_example.txt").as_slice()).unwrap(), [10605, 2713310158]);
     }
     #[test]
     fn input() {
-        assert_eq!(solve(include_bytes!("../../data/day11_input.txt").as_slice()).unwrap(), [110220]);
+        assert_eq!(solve(include_bytes!("../../data/day11_input.txt").as_slice()).unwrap(), [110220, 19457438264]);
     }
 }
